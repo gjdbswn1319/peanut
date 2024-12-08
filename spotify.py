e361cebf166908e934eb76acd214ae4cbcbf89b4
Elly Hur <elly@Ellys-MacBook-Air.local>
2024-12-08 22:11:14 +0900

    initial test of Spotify API

---
+++ spotify.py
@@ -0,0 +1,37 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id="048596e668f6462aac3d4c1e680cf083", 
+        client_secret="29f72ea1157c4026831a73e1b1047905"
+    )
+)
+
+
+
+
+def searchArtistIdByName():
+    results = sp.search(q='Imagine Dragons', limit=1)
+    return results["tracks"]["items"][0]["album"]["artists"][0]["id"]
+
+
+
+results = sp.search(q='imagine dragons', limit=1)
+artist_id = results["tracks"]["items"][0]["album"]["artists"][0]["id"]
+artist_data = sp.artist(artist_id)
+print(artist_data["name"])
+artist_album = sp.artist_albums(artist_id, limit=50)
+for album in artist_album["items"]:
+    print(album["name"])
+    print(album["release_date"])
+    print(album["images"][0]["url"])
+    print(album["album_type"])
+
+
+
+'''
+def searchArtistIdByName:
+    results = sp.search(q='Imagine Dragons', limit=1)
+    return results["items"][0]["id"]
+'''
